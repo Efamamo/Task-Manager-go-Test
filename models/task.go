392fa962a5898361646fa20a0cf5a744db858c65
ephrem <ephremmamo555@gmail.com>
2024-08-07 22:00:54 +0300

    update task model for database opration

--- models/task.go
+++ models/task.go
@@ -2,12 +2,14 @@ package model
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Task struct {
-	ID          string    `json:"id"`
-	Title       string    `json:"title" binding:"required"`
-	Description string    `json:"description" binding:"required"`
-	DueDate     time.Time `json:"due_date" binding:"required"`
-	Status      string    `json:"status" binding:"required"`
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
+	Title       string             `bson:"title" binding:"required" json:"title"`
+	Description string             `bson:"description" binding:"required" json:"description"`
+	DueDate     time.Time          `bson:"due_date" json:"due_date"`
+	Status      string             `bson:"status" binding:"required" json:"status"`
 }
