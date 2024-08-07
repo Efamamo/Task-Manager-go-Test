73253a4803848b3e38dfe0f4d97764309e56d771
ephrem <ephremmamo555@gmail.com>
2024-08-07 22:00:15 +0300

    update services to do mongodb crud opration

--- data/task_service.go
+++ data/task_service.go
@@ -1,75 +1,191 @@
 package data
 
 import (
+	"context"
 	"errors"
-	"strconv"
+	"fmt"
+	"log"
+	"strings"
 	"time"
 
 	model "github.com/Task-Management-go/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Mock data for tasks
-var tasks = []model.Task{
-	{ID: "1", Title: "Task 1", Description: "First task", DueDate: time.Now(), Status: "Pending"},
-	{ID: "2", Title: "Task 2", Description: "Second task", DueDate: time.Now().AddDate(0, 0, 1), Status: "In Progress"},
-	{ID: "3", Title: "Task 3", Description: "Third task", DueDate: time.Now().AddDate(0, 0, 2), Status: "Completed"},
-}
+var client *mongo.Client
+var collection *mongo.Collection
 
-func GetTasks() []model.Task {
-	return tasks
-}
+// Initialize MongoDB connection once
+func init() {
+	clientOptions := options.Client().ApplyURI("mongodb+srv://nest:efamamo@cluster0.avreuwg.mongodb.net/")
 
-func GetTaskById(ID string) *model.Task {
-
-	for i, val := range tasks {
-		if val.ID == ID {
-			return &tasks[i]
-		}
+	// Connect to MongoDB
+	var err error
+	client, err = mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
 	}
-	return nil
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("task-management").Collection("tasks")
+	fmt.Println("Connected to MongoDB!")
+}
+
+// Get the collection
+func GetTasks() (*[]model.Task, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+
+		return nil, err
+	}
+
+	defer cursor.Close(ctx)
+
+	var tasks []model.Task
+	for cursor.Next(ctx) {
+		var task model.Task
+		if err := cursor.Decode(&task); err != nil {
+
+			return nil, err
+		}
+		tasks = append(tasks, task)
+	}
+
+	if err := cursor.Err(); err != nil {
+
+		return nil, err
+	}
+
+	return &tasks, nil
+}
+
+func GetTaskByID(id string) (*model.Task, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// Convert the id string to a MongoDB ObjectID
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a filter to match the specific ID
+	filter := bson.M{"_id": objectID}
+
+	// Find a single document that matches the filter
+	var task model.Task
+	err = collection.FindOne(ctx, filter).Decode(&task)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, errors.New("task Not Found")
+		}
+		return nil, err
+	}
+
+	return &task, nil
+
 }
 
 func UpdateItem(ID string, updatedTask model.Task) (*model.Task, error) {
-	for i, val := range tasks {
-		if val.ID == ID {
-			if updatedTask.Title != "" {
-				tasks[i].Title = updatedTask.Title
-			}
-
-			if updatedTask.Description != "" {
-				tasks[i].Description = updatedTask.Description
-			}
-
-			if updatedTask.Status == "In Progress" || updatedTask.Status == "Completed" || updatedTask.Status == "Pending" {
-				tasks[i].Status = updatedTask.Status
-			} else if updatedTask.Status != "" {
-				return nil, errors.New("Status Error")
-			}
-
-			return &tasks[i], nil
-		}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	id, err := primitive.ObjectIDFromHex(ID)
+	if err != nil {
+		return nil, err
 	}
-	return nil, errors.New("Not Found")
+
+	task, err := GetTaskByID(ID)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(task)
+
+	if strings.ToLower(updatedTask.Status) != "in progress" && strings.ToLower(updatedTask.Status) != "completed" && strings.ToLower(updatedTask.Status) != "pending" {
+		return nil, errors.New("status error")
+	}
+	filter := bson.M{"_id": id}
+
+	update := bson.M{
+		"$set": bson.M{
+			"title":       updatedTask.Title,
+			"description": updatedTask.Description,
+			"due_date":    updatedTask.DueDate,
+			"status":      updatedTask.Status,
+		},
+	}
+
+	// Update the document that matches the filter
+	result, err := collection.UpdateOne(ctx, filter, update)
+	fmt.Println(result)
+	if err != nil {
+		return nil, err
+	}
+
+	utask, err := GetTaskByID(ID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return utask, nil
 
 }
 
-func DeleteTask(ID string) *model.Task {
-	for i, val := range tasks {
-		if val.ID == ID {
-			tasks = append(tasks[:i], tasks[i+1:]...)
-			return &val
-		}
+func DeleteTask(ID string) (*model.Task, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	id, err := primitive.ObjectIDFromHex(ID)
 
+	if err != nil {
+		return nil, err
 	}
-	return nil
+
+	task, err := GetTaskByID(ID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	filter := bson.M{"_id": id}
+
+	// Delete the document that matches the filter
+	result, err := collection.DeleteOne(ctx, filter)
+	fmt.Println(result)
+	if err != nil {
+		return nil, err
+	}
+
+	return task, nil
+
 }
 
 func AddTask(task model.Task) (*model.Task, error) {
-	id := strconv.Itoa(len(tasks) + 1)
-	if task.Status != "In Progress" && task.Status != "Completed" && task.Status != "Pending" {
-		return nil, errors.New("Status Error")
+
+	if strings.ToLower(task.Status) != "in progress" && strings.ToLower(task.Status) != "completed" && strings.ToLower(task.Status) != "pending" {
+		return nil, errors.New("status error")
 	}
-	task.ID = id
-	tasks = append(tasks, task)
+
+	task.ID = primitive.NewObjectID()
+
+	insertResult, err := collection.InsertOne(context.TODO(), task)
+	fmt.Println(insertResult)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return &task, nil
 }
